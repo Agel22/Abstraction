3ceff710bf72eb996e2576b6bdfb45895946f7d2
Agel22 <62006074+Agel22@users.noreply.github.com>
2020-06-13 21:20:51 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Encapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mahasiswa mhs = new Mahasiswa();
+            mhs.Nim = "2834";
+            mhs.Nama = "Agel suprianto";
+            mhs.Registrasi();
+
+            Console.ReadKey();
+        }
+    }
+}
